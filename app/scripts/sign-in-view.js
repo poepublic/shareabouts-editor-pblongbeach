1c4978243eae64a35982a0a2bce668b5a3d337e6
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-07-02 01:45:53 -0400

    Disable sign in button after clicking, and add error messages on unsuccessful login

--- app/scripts/sign-in-view.js
+++ app/scripts/sign-in-view.js
@@ -29,13 +29,30 @@ var Shareabouts = Shareabouts || {};
           username = this.$('#username').val(),
           password = this.$('#password').val();
 
+      self.$('.error').remove();
+      self.$('.sign-in-button').attr('disabled', 'disabled');
+
       NS.auth.requestNewUserSession(username, password, {
         success: function() {
           // Run the redirect callback
           self.redirect();
+        },
+        error: function($xhr) {
+          if ($xhr.status === 400 || $xhr.status === 401) {
+            var data = $xhr.responseJSON;
+            if (data.errors['__all__']) {
+              self.$('.sign-in-form').prepend('<small class="error">Incorrect username or password.</small>');
+            } else if (data.errors['username']) {
+              self.$('#username').after('<small class="error">You must enter a username.</small>');
+            } else if (data.errors['password']) {
+              self.$('#password').after('<small class="error">You must enter a password.</small>');
+            }
+          }
+        },
+        complete: function() {
+          self.$('.sign-in-button').removeAttr('disabled');
         }
       });
-
     }
   });
 
