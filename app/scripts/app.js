5c7bd3c4b875520c9bacbeb27c688e1219e88447
fkh <frank@openplans.org>
2014-08-14 13:40:55 -0400

    Adding download links

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,8 +16,9 @@ var Shareabouts = Shareabouts || {};
   // Initialize the dataset collection
   NS.app.addInitializer(function(options){
     NS.datasetCollection = new Backbone.Collection([
-      { id: 'abandoned-bikes', name: 'Abandoned Bikes', url: 'http://data.shareabouts.org/api/v2/chicagobikes/datasets/chicago-abandoned-bikes'},
-      { id: 'bike-parking', name: 'Bike Parking', url: 'http://data.shareabouts.org/api/v2/chicagobikes/datasets/chicago-bike-parking'}
+      { id: 'abandoned-bikes', name: 'Abandoned Bikes', url: 'http://data.shareabouts.org/api/v2/chicagobikes/datasets/chicago-abandoned-bikes', dataUrl: 'http://data.shareabouts.org/api/v2/chicagobikes/datasets/chicago-abandoned-bikes/places?include_invisible&include_private&page_size=5000&format=csv'
+},
+      { id: 'bike-parking', name: 'Bike Parking', url: 'http://data.shareabouts.org/api/v2/chicagobikes/datasets/chicago-bike-parking', dataUrl: 'http://data.shareabouts.org/api/v2/chicagobikes/datasets/chicago-bike-parking/places?include_invisible&include_private&page_size=5000&format=csv'}
     ]);
   });
 
